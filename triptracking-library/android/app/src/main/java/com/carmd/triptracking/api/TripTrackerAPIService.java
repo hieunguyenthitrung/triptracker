6c1168980ec773f4d9cc766f622361c3f818da87
Hieu Nguyen <hieu.nguyen@sw.innova.com>
2026-04-22 11:01:26 +0700

    Update new version 1.0.31

--- triptracking-library/android/app/src/main/java/com/carmd/triptracking/api/TripTrackerAPIService.java
+++ triptracking-library/android/app/src/main/java/com/carmd/triptracking/api/TripTrackerAPIService.java
@@ -277,13 +277,15 @@ public final class TripTrackerAPIService {
                 body.put("os_Info", osInfo);
                 body.put("location", locArr);
 
+                Log.d(TAG, "Ping vehicle " + vehicleId + "- routeId: " + routeId + "- includeVehicleId: " + includeVehicleId   );
+
                 // Only include vehicle_Id during active trip and if configured
                 if (includeVehicleId && !vehicleId.isEmpty()) {
                     body.put("vehicle_Id", vehicleId);
-                    body.put("route_Id", routeId != null ? routeId : this.routeId);
+                    body.put("route_Id", vehicleId);
                     Log.d(TAG, "Ping route_Id: " + routeId != null ? routeId : this.routeId);
                 }else{
-                    Log.d(TAG, "Ping without vehicle_IdvehicleId: " + vehicleId);
+                    Log.d(TAG, "Ping without vehicle_IdvehicleId: " + vehicleId + "- routeId: " + routeId + "- includeVehicleId: " + includeVehicleId   );
                 }
 
                 boolean ok = post(pingURL, body);
