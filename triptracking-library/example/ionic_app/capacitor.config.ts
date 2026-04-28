953459544b332260153da26b259bba63f0e7d688
Hieu Nguyen <hieu.nguyen@sw.innova.com>
2026-04-28 10:18:50 +0700

    Revert "Reset fake location"
    This reverts commit 6c9b0c29b59767d583a879c0ad762dd89666f93f.

--- triptracking-library/example/ionic_app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'example/ionic_app',
-  webDir: 'www'
-};
-
-export default config;
